2651cf26b564c7e66976b74a933b54b0f2ba2a7d
Nick Welna <welnanick@gmail.com>
2017-10-08 22:47:00 -0700

    master: Lesson 5 additions + improvements

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,34 +3,63 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
+    private int quantity = 2;
+    private int pricePerCup = 5;
+    private boolean hasWhippedCream = false;
+    private boolean hasChocolate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void increment(View view) {
 
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
 
     }
 
     public void decrement(View view) {
 
         quantity--;
-        display(quantity);
+        displayQuantity(quantity);
+
+    }
+
+    /**
+     * Create a summary of the order
+     *
+     * @param orderPrice      The price of the order
+     * @param hasWhippedCream Does the user have Whipped Cream
+     * @param hasChocolate    Does the user have Chocolate
+     * @param name            The users name
+     * @return The summary of the order
+     */
+    public String createOrderSummary(int orderPrice, boolean hasWhippedCream, boolean hasChocolate, String name) {
+        String orderSummary = "Name: " + name + "\n";
+        orderSummary += "Add whipped cream?: " + hasWhippedCream + "\n";
+        orderSummary += "Add chocolate?: " + hasChocolate + "\n";
+        orderSummary += "Quantity: " + quantity + "\n";
+        orderSummary += "Total: " + NumberFormat.getCurrencyInstance().format(orderPrice) + "\n";
+        orderSummary += "Thank You!";
+        return orderSummary;
+
 
     }
 
@@ -38,23 +67,56 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+
+        EditText nameEditText = findViewById(R.id.name_edit_text);
+        String name = nameEditText.getText().toString();
+
+        displaySummary(createOrderSummary(calculatePrice(), hasWhippedCream, hasChocolate, name));
+
+    }
+
+    /**
+     * Calculates the price of the order.
+     */
+    private int calculatePrice() {
+
+        return quantity * pricePerCup;
+
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+    private void displayQuantity(int numberOfCups) {
+
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.format(Locale.US, "%d", numberOfCups));
+
     }
 
 
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void displaySummary(String summary) {
+
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(summary);
+
     }
+
+    public void updateWhippedCream(View view) {
+
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_check_box);
+        hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+    }
+
+    public void updateChocolate(View view) {
+
+        CheckBox chosolateCheckBox = findViewById(R.id.chocolate_check_box);
+        hasChocolate = chosolateCheckBox.isChecked();
+
+    }
+
 }
